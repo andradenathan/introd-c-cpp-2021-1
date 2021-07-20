dd8ac8b7f4fa0031ecc46db08361ece1a6bd41a7
Nathan Andrade <nathan.nhdsk@gmail.com>
2021-07-19 23:41:07 -0300

    Desafio da Forca

---
+++ forca.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char palavra_secreta[20];
+
+  sprintf(palavra_secreta, "BISCOITO");
+
+  int acertou = 0;
+  int enforcou = 0;
+
+  char chutes[26];
+  int tentativas = 0;
+
+
+  do {
+    for(int i = 0; i < strlen(palavra_secreta); i++) {
+
+      int acertou = 0;
+      
+      for(int j = 0; j < tentativas; j++) {
+        if(chutes[j] == palavra_secreta[i]) {
+          acertou = 1;
+          break;
+        }
+      }
+
+      if(acertou) {
+        printf("%c ", palavra_secreta[i]);
+
+      } else {
+         printf("_ ");
+      }
+    }
+  
+    printf("\n");
+
+    char chute;
+    scanf(" %c", &chute);
+    
+    chutes[tentativas] = chute; 
+    tentativas++;
+
+  } while(!acertou && !enforcou);
+}
